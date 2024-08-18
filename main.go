7bad2afe1597a560ac5c28a216d1c26a9c9b3c49
Mariusz Borowiak <142698842+borowiak-m@users.noreply.github.com>
2024-08-18 12:43:35 +0100

    init main

--- main.go
+++ main.go
@@ -1,3 +1,15 @@
 package main
 
-func main() {}
+import (
+	"log"
+	"net/http"
+)
+
+func serveIndex(w http.ResponseWriter, req *http.Request) {}
+
+func main() {
+	http.HandleFunc("/", serveIndex)
+	http.HandleFunc("/ws", nil)
+
+	log.Fatal(http.ListenAndServe(":3000", nil))
+}
